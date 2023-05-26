edc0ff077e2e2aadad5c7e756acf9b97b2522b25
Asadbek0905 <asadbekurozmatov965@gmail.com>
2023-05-26 19:31:22 +0500

    fix

---
+++ index.js
@@ -0,0 +1,22 @@
+let inp = document.querySelectorAll('.form-control');
+let btn = document.querySelector('.btn');
+
+btn.onclick = () => {
+
+   let inputValueOne = inp[0].value
+   let inputValueTwo = inp[1].value
+   let obj = {name: inputValueOne, lastname: inputValueTwo}
+
+   fetch('https://asadbek-instagram.onrender.com/data', {
+       method: 'POST',
+       headers: {
+           'Content-Type': 'application/json'
+       },
+       body: JSON.stringify(obj)
+   }).then(res => {
+       res.status === 201 ? alert('Succes') : alert('error')
+       inp[0].value = ''
+       inp[1].value = ''
+   })
+
+}
